b540730e3ad8925c8cd2f91b64aa80dfd23552eb
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-04 19:09:42 +0000

    Aula finalizada

---
+++ Aula1.java
@@ -0,0 +1,5 @@
+public class Aula1 {
+    public static void main (String[] args){//inicio do algoritmo.
+        System.out.println("Bom dia, mundo");//system/sistema; out/saida - envia informação - mostrar na tela o que eu quero.
+    }
+}
