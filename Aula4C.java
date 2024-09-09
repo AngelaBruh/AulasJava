d8808d4dd450c9ddbb4144e2a4b0259a1360aaed
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-09 19:33:09 +0000

    Aula finalizada

--- Aula4C.java
+++ Aula4C.java
@@ -16,7 +16,24 @@ public class Aula4C {
         System.out.printf("%2f\n", resultado);
         //Multiplicação
         resultado = numeroNaoQuebrado * numeroQuebrado;
-        System.out.printf("%2f\n", resultado);     
+        System.out.printf("%2f\n", resultado);   
+        letra = 'A'; //representa uma unidade de caracter, mas é possivel limitar a quantidade.
+        texto = "ABC - ABCdario";
+        System.out.println(texto.charAt(4));
+        System.out.println(texto+texto+texto);
+        teste = letra == 'A'; // == comparação
+        System.out.println(teste);
+        teste = resultado > 1;
+        System.out.println(teste);
+        teste = resultado < 10;
+        System.out.println(teste);
+        teste = resultado >= 5;
+        System.out.println(teste);
+
+
+
+
+        
 
     }
 }
