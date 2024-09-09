be7e3ac0bb8ccef669e5c497376eefe7981a270c
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-09 20:19:59 +0000

    Aula finalizada

--- Exe1Aula4.java
+++ Exe1Aula4.java
@@ -3,7 +3,26 @@
  */
 
 
-
+import java.util.Scanner;
 public class Exe1Aula4 {
-    
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Informe o nome do aluno: ");
+        String nomeAluno = leitura.nextLine();
+        System.out.println("Informe a primeira nota: ");
+        double nota1 = leitura.nextDouble();
+        System.out.println("Informe a segunda nota: ");
+        double nota2 = leitura.nextDouble();
+        System.out.println("Informe a terceira nota: ");
+        double nota3 = leitura.nextDouble();
+        System.out.println("Informe a quarta nota: ");
+        double nota4 = leitura.nextDouble();
+        System.out.println("Informe a quinta nota: ");
+        double nota5 = leitura.nextDouble();
+        
+        double media = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5);
+        System.out.println("Media final do aluno(a) " + nomeAluno + ": " + media);
+        leitura.close();
+        
+    }
 }
