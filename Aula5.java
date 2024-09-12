dc2045b9f6e67f3563b8c37a2d06e665d6e23730
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-12 11:40:58 +0000

    Aula finalizada

--- Aula5.java
+++ Aula5.java
@@ -3,7 +3,9 @@
 
 import java.util.Scanner;
 import java.util.Random;
+
 public class Aula5 {
+   
     public static void main(String[] args) {
         //Atribuindo valores iniciais
         Random aleatorio = new Random();
@@ -16,14 +18,52 @@ public class Aula5 {
 
         //Testes de pontuação
         if (numero == sorteio) {
-            System.out.println("Você acertou com " + pontuacao + "pontos");
+            System.out.println("Você acertou com " + pontuacao + " pontos");
             leitura.close();
             return;
         } else {
             pontuacao = pontuacao - 10;
             System.out.println("Você perdeu " + 10 + " pontos e está com " + pontuacao + " pontos"); //Aqui não necessita de return pois é a saida
+            if (pontuacao == sorteio) {
+                System.out.println("Você acertou com " + pontuacao + "pontos");
+                leitura.close();
+                return;
+            } else {
+                pontuacao = pontuacao - 10;
+                System.out.println("Você perdeu " + 10 + " pontos e está com " + pontuacao + " pontos"); 
+            }
+            if (pontuacao == sorteio) {
+                System.out.println("Você acertou com " + pontuacao + " pontos");
+                leitura.close();
+                return;
+            } else {
+                pontuacao = pontuacao - 10;
+                System.out.println("Game Over - Você está fora do jogo"); 
+            }
+        }       
 
-        }
+       
+         /*/Usuario perdeu 10 pontos, possuindo 20
+         if (pontuacao == sorteio) {
+            System.out.println("Você acertou com " + pontuacao + "pontos");
+            leitura.close();
+            return;
+        } else {
+            pontuacao = pontuacao - 10;
+            System.out.println("Você perdeu " + 10 + " pontos e está com " + pontuacao + " pontos"); 
+        }*/
+
+
+         /*/Usuario perdeu 20 pontos, possuindo 10
+         if (pontuacao == sorteio) {
+            System.out.println("Você acertou com " + pontuacao + " pontos");
+            leitura.close();
+            return;
+        } else {
+            pontuacao = pontuacao - 10;
+            System.out.println("Game Over - Você está fora do jogo"); 
+        }*/
+
         
         leitura.close();
     }
