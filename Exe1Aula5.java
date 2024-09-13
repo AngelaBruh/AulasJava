e8040d1226055e74e016d7a227138fae44687be4
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-13 11:43:16 +0000

    Aula finalizada

---
+++ Exe1Aula5.java
@@ -0,0 +1,16 @@
+/*
+ * Identifique os erros do código e corrija-os:
+ * public class Main {public static void main(String[] args) {int a=5b=10,c c=+ab;System.out.println(c);}}
+ * 
+ */
+
+
+public class Exe1Aula5 {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int c;
+
+        c = a + b;
+        System.out.println(c);
+    }
+}
