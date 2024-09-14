055709d3e38e5ab42494d1c2bf92622f2873a182
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-14 17:38:30 +0000

    Aula finalizada

---
+++ Exe3Aula5.java
@@ -0,0 +1,21 @@
+/* 
+ * Crie um sistema que leia a temperatura em graus Celsius e determine se ela é quente ou fria.
+ */
+
+
+
+import java.util.Scanner;
+public class Exe3Aula5 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite uma temperatura: ");
+        int temp = leitor.nextInt();
+
+        if (temp >= 30) {
+            System.out.println("Tá calor, beba agua.");
+        } else {
+            System.out.println("Tá frio, vá se aquecer.");
+        }
+    }
+}
