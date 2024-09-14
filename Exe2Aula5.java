73da81aae2a9d12e228c9290aff559724c3a00a0
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-14 15:35:02 +0000

    Aula finalizada

---
+++ Exe2Aula5.java
@@ -0,0 +1,42 @@
+/*
+ * Calcule a soma das médias dos 5 estudantes e, ao final, informe se cada estudante foi 
+ * aprovado ou reprovado (considere que a média de aprovação deve ser maior ou igual a 6).
+ * 
+ */
+
+    import java.util.Scanner;
+public class Exe2Aula5 {
+    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double[] notas = new double[5];
+        String[] nomes = new String[5];
+        double[] media = new double[5];
+        double soma = 0;
+
+        for(int i = 0; i < 5; i++){
+        System.out.println("Informe o nome do aluno(a): ");
+        nomes[i] = leitor.nextLine();
+
+            System.out.println("Informe as notas do aluno(a) " + nomes[i] + ":");
+            for(int n = 0; n < 5; n++){            
+                notas[i] = leitor.nextDouble();
+                soma += notas[i];
+            }   
+            leitor.nextLine();
+
+            media[i] = soma/5;                       
+            if (media[i] >= 6.0) {
+                System.out.println("Aluno(a) " + nomes[i] + ": aprovado.");
+                /*leitor.close(); não coloque esses, pois fechar o programa e não vai ler o proximo aluno, e ocasionar erro */
+                  return;
+            } else {
+                System.out.println("Aluno(a) " + nomes[i] + ": reprovado.");
+            }
+       
+        }    
+        
+       
+        leitor.close();
+    }
+}
