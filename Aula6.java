037d2ada421addd714909c4006a65f3d0873eb0a
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-16 18:03:35 +0000

    Aula finalizada

--- Aula6.java
+++ Aula6.java
@@ -9,12 +9,16 @@
                 Random aleatorio = new Random();
                 Scanner leitura = new Scanner(System.in);
                 final int sorteio = aleatorio.nextInt(1,6);
+                int pontuacao = 30;
                 System.out.println("Digite um numero: ");
                 int numero = leitura.nextInt();
                 if(sorteio < numero){
-                    System.out.println("Numero digitado é maior que o numero selecionado.");
+                    //pontuacao = pontuacao - 10
+                    pontuacao -= 10;
+                    System.out.println("Numero digitado é maior que o numero sorteado e você está com " + pontuacao + "pontos");
                 }else if(sorteio > numero){
-                    System.out.println("Numero digitado é menor que o numero selecionado.");
+                    pontuacao -= 10;
+                    System.out.println("Numero digitado é menor que o numero sorteado e você está com " + pontuacao + "pontos");
                 }else{
                     System.out.println("Você acertou com 30 pontos");
                     leitura.close(); //finalizar o programa antes do return
@@ -27,6 +31,6 @@
 
 
             }
-            
+
 
  }
