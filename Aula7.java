c6a5bd3953fb2b7648b9fcdb6a9fff59d5533306
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-18 17:50:45 +0000

    Aula finalizada

--- Aula7.java
+++ Aula7.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 import java.util.Random;
 public class Aula7{
 
-        public static void main(String[] args) { //while - enquanto
+        public static void main(String[] args) { 
             Random aleatorio = new Random();
             Scanner leitura = new Scanner(System.in);
             final int sorteio = aleatorio.nextInt(1,6);
-            int pontuacao = 30;
+            int pontuacao = 50;
             System.out.println("Digite um numero: ");
-            int numero = leitura.nextInt();
-            if(sorteio < numero){
-                //pontuacao = pontuacao - 10
-                pontuacao -= 10;
-                System.out.println("Numero digitado é maior que o numero sorteado e você está com " + pontuacao + "pontos");
-            }else if(sorteio > numero){
-                pontuacao -= 10;
-                System.out.println("Numero digitado é menor que o numero sorteado e você está com " + pontuacao + "pontos");
-            }else{
-                System.out.println("Você acertou com 30 pontos");
-                leitura.close(); 
-                return;
+            int numero;
+            while (pontuacao > 0) { //while - enquanto
+                    numero = leitura.nextInt();
+                    if(sorteio < numero){
+                        //pontuacao = pontuacao - 10
+                        pontuacao -= 10;
+                        System.out.println("Numero digitado é maior que o numero sorteado e você está com " + pontuacao + "pontos");
+                    }else if(sorteio > numero){
+                        pontuacao -= 10;
+                        System.out.println("Numero digitado é menor que o numero sorteado e você está com " + pontuacao + "pontos");
+                    }else{
+                        System.out.println("Você acertou com 30 pontos");                
+                        return;
+                    }
+
             }
-            leitura.close();              
-
-
-
-
+            System.out.println("Você perdeu :(");          
+            leitura.close();      
 
         }
 
