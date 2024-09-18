92640cfb63682e8e58b5e93d952e524bbaffe7d8
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-18 19:47:37 +0000

    Aula finalizada

--- Exe2Aula7.java
+++ Exe2Aula7.java
@@ -6,17 +6,27 @@ public class Exe2Aula7 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         String nomeAluno;
+        int nota1, nota2;
+
         System.out.println("Informe o nome do aluno(a): ");
         nomeAluno = leitor.nextLine();
-        for(int i; i > 2; i++){
-        System.out.println(" Informe as duas nota do aluno(a) " + nomeAluno);
-        }
+        System.out.println("Informe as duas nota do aluno(a) " + nomeAluno);
+
+        System.out.println("Digite a primeira nota:");
+        nota1 = leitor.nextInt();
+        System.out.println("Digite a segunda nota:");
+        nota2 = leitor.nextInt();
+
+        double media = (nota1+nota2)/2;
+
+        System.out.println("Media do aluno(a) " + nomeAluno + " foi: " + media);   
 
 
 
 
 
 
+    leitor.close();
     }
 
 }
