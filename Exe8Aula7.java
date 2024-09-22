7e051ad8cbfa728b105941c600268fc9844d404f
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-22 15:58:40 +0000

    Aula finalizada

--- Exe8Aula7.java
+++ Exe8Aula7.java
@@ -2,6 +2,33 @@
  * Desenvolva um sistema que leia a quantidade de produtos e o valor unitário, retornando o valor total.
  */
 
+ import java.util.Scanner;
 public class Exe8Aula7 {
-    
-}
+    public static void main(String[] args) {
+            Scanner leitor = new Scanner(System.in);
+            double valorTotal = 0.0;
+            String continuar = "s";
+            String produto;
+
+        for (; continuar.equalsIgnoreCase("s");){ //equalsIgnoreCase - ele ignora o fato da letra ser maiuscula ou minuscula
+            System.out.println("Informe o produto: ");
+            produto = leitor.nextLine();
+
+            System.out.print("Digite a quantidade de " + produto + ":");
+            int quantidade = leitor.nextInt();
+
+            System.out.print("Digite o valor unitário de " + produto + ":");
+            double valorUnitario = leitor.nextDouble();
+
+            valorTotal += quantidade * valorUnitario;
+
+            System.out.print("Deseja adicionar mais produtos? (s/n): ");
+            continuar = leitor.next();
+            leitor.nextLine();
+        }
+
+        System.out.println("O valor total é: R$ " + valorTotal);
+        leitor.close();
+    }
+
+}          
--- Exe9Aula7.java
+++ Exe9Aula7.java
@@ -1,7 +1,21 @@
 /*
  * Implemente um programa que some valores inseridos pelo usuário até que ele digite 0, parando de rodar o programa.
  */
-
+import java.util.Scanner;
 public class Exe9Aula7 {
-    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int soma = 0;
+        int valor = -1; // Inicializa com um valor diferente de 0
+
+        System.out.println("Digite 0 para interromper a operação.");
+        while (valor != 0) {
+            System.out.print("Digite um valor: ");
+            valor = leitor.nextInt();
+            soma += valor;
+        }
+
+        System.out.println("A soma dos valores é: " + soma);
+        leitor.close();
+    }
 }
