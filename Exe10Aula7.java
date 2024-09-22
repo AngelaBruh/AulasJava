3008d1b7e1d3f5110753b4796516c0312f7fb8bb
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-22 15:09:13 +0000

    Aula finalizada

---
+++ Exe10Aula7.java
@@ -0,0 +1,42 @@
+/*
+ * Localize e ajuste os erros no código abaixo:
+public class CheckNumber {
+        public static void main(String[] args) {
+            int number = 10;
+
+            if (number > 0) {
+                System.out.println("Number is positive")
+            else {
+                System.out.println("Number is negative or zero";
+            }
+
+            if (number < 5
+                System.out.println("O número é menor que 5");
+            } else {
+                System.out.println("Number is 5 or more);
+            }
+        }
+}
+ */
+
+
+
+public class Exe10Aula7 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("O numero é positivo");
+        }
+        else {
+            System.out.println("O numero é negativo ou zero");
+        }
+
+        if (number < 5){
+            System.out.println("O numero é menor que 5");
+        }
+         else{
+            System.out.println("O numero é maior que 5");
+        }
+    }
+}
