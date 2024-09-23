96cf853fba7f9c2637ed902a55721c6e4e84a3e5
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-23 20:03:53 +0000

    Aula finalizada

--- Aula8.java
+++ Aula8.java
@@ -1,14 +1,17 @@
-/*
- * Faça a leitura de 30 pessoas e informe em numeros pares os registros
- * 
- */
-
-
-
-
+import java.util.Scanner;
 
 public class Aula8 {
     public static void main(String[] args) {
-        String
+        //Faça a leitura de 30 pessoas e informe em numeros pares os registros 
+        String[] pessoa = new String[30];
+        Scanner leitura = new Scanner(System.in);
+        for (int i = 0; i < pessoa.length; i++) {
+            System.out.println("Digite um nome: ");
+            pessoa[i] = leitura.nextLine();
+        }
+        leitura.close();        
+        for (int i = 1; i < pessoa.length; i= i+2) {
+            System.out.println(pessoa[i]);
+        }
     }
 }
