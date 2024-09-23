2ea05c120adf67ad65feccd061b4a3ad26a8019c
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-23 17:07:23 +0000

    Aula finalizada

--- Exe5Aula7.java
+++ Exe5Aula7.java
@@ -6,7 +6,17 @@
  * 
  *                          No terminal do codespace, verifique a versão atual:
  *                                       java -version
- *
+ *                              Atualize os pacotes do sistema:
+ *                                  sudo apt-get update
+ *                                  sudo apt-get upgrade
+ *                              Instale a versão mais recente:
+ *                              sdk install java 17.0.9-amzn
+ *                             sudo apt-get install openjdk-22-jdk
+ *           Verifique qual é a versão mais recente disponível nos repositórios. Você pode listar as versões disponíveis com:
+ *                                 apt-cache search openjdk
+ * ===============================================================================================================================
+ * 
+ * 
  * 
  * 
  * 
