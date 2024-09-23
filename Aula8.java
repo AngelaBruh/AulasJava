5aaa106916006c42079dfc85360f38c0b1880bb7
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-23 19:33:13 +0000

    Aula finalizada

---
+++ Aula8.java
@@ -0,0 +1,14 @@
+/*
+ * Faça a leitura de 30 pessoas e informe em numeros pares os registros
+ * 
+ */
+
+
+
+
+
+public class Aula8 {
+    public static void main(String[] args) {
+        String
+    }
+}
