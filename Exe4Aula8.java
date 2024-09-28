3c02fda73c74798559633208f713e31568a21e08
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-28 19:48:22 +0000

    Aula Finalizada

--- Exe4Aula8.java
+++ Exe4Aula8.java
@@ -1,3 +1,21 @@
 /*
  * Crie um programa em Java que imprima apenas os múltiplos de 3 e 7, até 100 vezes.
  */
+
+ public class Exe4Aula8 { 
+    public static void main(String[] args) {
+        int tabu3, tabu7;
+        System.out.println("Multiplos 3 até 100 vezes:");
+        for(int i = 0; i<=100 ;i++){
+            tabu3 = 3 * i;
+            System.out.println(tabu3);
+        }
+        System.out.println("Multiplos 7 até 100 vezes:");
+        for(int i = 0; i<=100 ;i++){
+            tabu7 = 7 * i;
+            System.out.println(tabu7);
+        }
+              
+        
+    }
+ }
--- Exe5Aula8.java
+++ Exe5Aula8.java
@@ -2,3 +2,20 @@
  * Faça um programa em Java para ler 50 números e armazenar em um vetor VET, 
  * verificar e escrever se existem números repetidos no vetor VET e em que posições se encontram.
  */
+
+ import java.util.Scanner;
+ public class Exe5Aula8 { 
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int[] VET = new int[50];
+
+        System.out.println("Digite até 50 numeros aleatorios:");
+        for(int i = 0; i < VET.length; i++){            
+            VET[i] = leitor.nextInt();          
+        }
+        
+        
+
+        leitor.close();
+    }
+ }
