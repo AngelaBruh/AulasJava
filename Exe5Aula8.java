90ad41b13707f802a7e31c3bca4c2106450d6bc3
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-09-29 13:20:11 +0000

    Aula finalizada

--- Exe5Aula8.java
+++ Exe5Aula8.java
@@ -13,7 +13,19 @@
         for(int i = 0; i < VET.length; i++){            
             VET[i] = leitor.nextInt();          
         }
-        
+
+        boolean repetidoEncontrado = false;
+        for (int i = 0; i < 50; i++) { //aqui estou clonando a lista,aqui é a primeira lista
+            for (int j = i + 1; j < 50; j++) { // aqui é a segunda lista
+                if (VET[i] == VET[j]) { // aqui vou estar comparando a lista
+                    repetidoEncontrado = true;
+                    System.out.println("Número repetido " + VET[i] + " encontrado nas posições " + i + " e " + j);
+                }
+            }
+        }
+        if (!repetidoEncontrado) {
+            System.out.println("Nenhum número repetido encontrado.");
+        }
         
 
         leitor.close();
