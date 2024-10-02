49e63b8c21fabf9421579275c3701ab05171f5cd
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2024-10-02 18:00:18 +0000

    Aula finalizada

--- Aula10.java
+++ Aula10.java
@@ -8,5 +8,6 @@ public class Aula10 {
     public static void main(String[] args) {
         FabricaMoto moto = new FabricaMoto();
         System.out.println(moto.nome);
+        moto.andar();
     }
 }
--- FabricaMoto.java
+++ FabricaMoto.java
@@ -1,13 +1,39 @@
-import java.time.LocalDateTime;
+
+
+
+
+
+//import java.time.LocalDateTime;
 import java.util.Scanner;
-public class FabricaMoto {
+public class FabricaMoto {    
+    public String nome;
     public String marca;
     public String cor;
-    public LocalDateTime ano;
-    public String nome;
-    public FabricaMoto(){
-        Scanner leitura = new Scanner(System.in);
-        this.nome = leitura.nextLine();
+    public int ano;     //public LocalDateTime ano;
+        public void andar(){ //função
+            System.out.println("Sua moto está andando.");
+        }
+
+    public FabricaMoto(){              
+            Scanner leitura = new Scanner(System.in);
+            System.out.println("Digite a marca da moto: ");
+            this.nome = leitura.nextLine();
+            System.out.println("Digite a cor da moto: ");
+            this.cor = leitura.nextLine();
+            while(true){
+                try{
+                    Scanner temp = new Scanner(System.in);
+                    System.out.println("Digite o ano da moto: ");
+                    this.ano = temp.nextInt();
+                    temp.close();
+                    break;
+                }
+                catch(Exception e){
+                    System.out.println("Digite um valor valido: ");
+                    continue;
+                }
+        }
+        leitura.close();
     }
 }
 
